0d23c3cc1dd2202aa171c7b63eaba7314cff8047
Cristhian Richard <criiss13.-@hotmail.com>
2016-11-02 18:59:05 -0300

    EJ4 y 5 sin comentar

---
+++ TP4/EJ4/Cuentas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ2
+{
+    class Cuentas
+    {
+        private Cuenta iCajaAhorro;
+
+        /// <summary>
+        /// propiedad de solo lectura del atributo Caja de ahorro de Cuentas
+        /// </summary>
+        public Cuenta CajaAhorro
+        {
+            get { return this.iCajaAhorro; }
+        }
+
+        private Cuenta iCuentaCorriente;
+        
+        /// <summary>
+        /// propiedad de solo lectura del atributo Cuenta corriente de Cuentas
+        public Cuenta CuentaCorriente
+        {
+            get { return this.iCuentaCorriente; }
+        }
+
+        private Cliente iCliente;
+       
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Cuentas()
+        {
+            this.iCajaAhorro =new Cuenta(1000,200);
+            this.iCuentaCorriente = new Cuenta(300,200);
+            this.iCliente= new Cliente(0,"38171725", "Stuart Dolittle");
+        }
+        
+    }
+}
---
+++ TP4/EJ4/FachadaCuentas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2
+{
+    class FachadaCuentas
+    {
+        private Cuentas iCuenta;
+
+        public Cliente CrearCliente(string nombre, string nroDoc, TipoDocumento tipoDoc)
+        {
+            Cliente cliente = new Cliente(tipoDoc, nroDoc, nombre);
+            return cliente;
+
+        }
+
+        public Cuenta CrearCuenta(double pAcuerdo, double pSaldo)
+        {
+                Cuenta cuenta = new Cuenta(pAcuerdo, pSaldo);
+                return cuenta;
+
+        }
+        public Cuenta CrearCuenta(double pAcuerdo)
+        {
+                Cuenta cuenta = new Cuenta(pAcuerdo);
+                return cuenta;
+        }
+
+
+        public void CrearCuentas(Cliente pCliente, Cuenta pCuentaCorriente, Cuenta pCajaAhorro)
+        {
+            this.iCuenta = new Cuentas(pCliente, pCuentaCorriente, pCajaAhorro);
+        }
+
+        public Cuenta ConsultarCC()
+        {
+            return this.iCuenta.CuentaCorriente;
+        }
+        public Cuenta ConsultarCA()
+        {
+            return this.iCuenta.CajaAhorro;
+        }
+        public Boolean TransferirCCaCA(double monto)
+        {
+            Boolean b;
+            b = this.iCuenta.CuentaCorriente.DebitarSaldo(monto);
+            if (b == true)
+            {
+                this.iCuenta.CajaAhorro.AcreditarSaldo(monto);
+            }
+            return b;
+        }
+        public Boolean TransferirCAaCC(double monto)
+        {
+            Boolean b;
+            b = this.iCuenta.CajaAhorro.DebitarSaldo(monto);
+            if (b == true)
+            {
+                this.iCuenta.CuentaCorriente.AcreditarSaldo(monto);
+            }
+            return b;
+        }
+        public Boolean DebitarCC(double monto)
+        {
+            return this.iCuenta.CuentaCorriente.DebitarSaldo(monto);
+        }
+        public Boolean DebitarCA(double monto)
+        {
+            return this.iCuenta.CajaAhorro.DebitarSaldo(monto);
+        }
+        public void AcreditarCA(double monto)
+        {
+            this.iCuenta.CajaAhorro.AcreditarSaldo(monto);
+        }
+        public void AcreditarCC(double monto)
+        {
+            this.iCuenta.CuentaCorriente.AcreditarSaldo(monto);
+        }
+
+    }
+}
