75f64036952fd603e29004c59eb8afb014404ba9
Lia Lucia Aguirre <aguirrel2011@gmail.com>
2016-11-07 16:18:21 -0300

    Todavia no esta terminado el 7 pero sino pusheo no me deja sincronizar jejeje

---
+++ TP4/EJ7/Calendario.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ7
+{
+    class Calendario
+    {
+        List<Evento> iListaEvento;
+        String iTitulo;
+        DateTime iFechaCreacion;
+
+        public Calendario(String pTitulo)
+        {
+            iListaEvento = new List<Evento>();
+            iFechaCreacion = DateTime.Now;
+            iTitulo = pTitulo;
+        }
+
+        public string Titulo
+        {
+            get { return this.iTitulo; }
+            set { this.iTitulo = value; }
+        }
+        public DateTime FechaCreacion
+        {
+            get { return this.iFechaCreacion; }
+            set { this.iFechaCreacion = value; }
+        }
+        
+        public List<Evento> Evento
+        {
+            get { return this.iListaEvento; }
+        }
+
+
+        public void AgregarEvento(Evento pEvento)
+        {
+                this.Evento.Add(pEvento);
+        
+        }
+            
+        
+        public void EliminarEvento(Evento pEvento) 
+        {
+            this.Evento.Remove(pEvento);
+
+        }
+
+        public void ModificarCalendario(string pTitulo)
+        {
+            this.iTitulo = pTitulo;
+            
+        }
+
+        public string ListarEvento()
+        {
+            string variable = "- ";
+
+            foreach (Evento elemento in this.Evento)
+            {
+                variable = variable + elemento.Titulo + " - ";
+
+            }
+
+            return variable;
+        }
+
+        public Calendario FiltrarPor(string pTituloEvento)
+        {
+            Calendario eventos = new Calendario(pTituloEvento);
+            foreach (Evento elemento in this.Evento)
+            {
+                if (elemento.Titulo == pTituloEvento)
+                {
+                    eventos.AgregarEvento(elemento);
+                }
+            }
+            return eventos;
+        }
+
+
+
+        public Calendario FiltrarPor(DateTime pFecha1, DateTime pFecha2)
+        {
+            Calendario eventos = new Calendario("EventosPorFechas");
+            foreach (Evento elemento in this.Evento)
+            {
+                if (elemento.FechaInicio>= pFecha1 && elemento.FechaFin<=pFecha2
+                    && elemento.FechaFin<=pFecha2 && elemento.FechaInicio>=pFecha1)
+                {
+                    eventos.AgregarEvento(elemento);
+                }
+            }
+            return eventos;
+        }
+
+        // iListaEvento.Find(Evento => Evento == pEvento);
+
+
+
+    }
+}
---
+++ TP4/EJ7/Evento.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ7
+{
+    class Evento
+    {
+        string iTitulo;
+        DateTime iFechaInicio;
+        DateTime iFechaFin;
+        TipoFrecuencia iFrecuencia;
+
+        public Evento(string pTitulo, DateTime pFechaInicio, DateTime pFechaFin, TipoFrecuencia pFrecuencia)
+        {
+            iTitulo = pTitulo;
+            if (pFechaFin < pFechaInicio)
+            {
+                FechaFinInvalidaException Exception = new FechaFinInvalidaException("La Fecha de Fin de evento ingresada es inválida");
+                throw Exception;
+
+            }
+            else
+            {
+                iFechaFin = pFechaFin;
+            }
+            
+            if (pFechaInicio < DateTime.Today)
+            {
+                FechaInicioInvalidaException Excepcion = new FechaInicioInvalidaException("La Fecha de Inicio de evento ingresada es inválida");
+                throw Excepcion;
+            }
+           
+            else
+            {
+                iFechaInicio = pFechaInicio;
+            }
+
+            iFrecuencia = pFrecuencia;
+        }
+
+        public string Titulo
+        {
+            get { return this.iTitulo; }
+            set { this.iTitulo = value; }
+        }
+
+        public DateTime FechaInicio
+        {
+            get { return this.iFechaInicio; }
+            set { this.iFechaInicio = value; }
+        }
+        public DateTime FechaFin
+        {
+            get { return this.iFechaFin; }
+            set { this.iFechaFin = value; }
+        }
+
+        public TipoFrecuencia Frecuencia
+        {
+            get { return this.iFrecuencia; }
+            set { this.iFrecuencia = value; }
+            
+        }
+
+        public void ModificarEvento(string pTitulo)
+        {
+            this.Titulo = pTitulo;
+        }
+        public void ModificarEvento(DateTime pFechaInicio, DateTime pFechaFin)
+        {
+
+            if (pFechaFin > pFechaInicio)
+            {
+                this.FechaFin = pFechaFin;
+            }
+            else
+            {
+                FechaFinInvalidaException Excepcion = new FechaFinInvalidaException("La Fecha de Fin de evento ingresada es inválida");
+                throw Excepcion;
+
+            }
+
+            if (this.FechaInicio > DateTime.Now)
+            {
+                this.FechaInicio = pFechaInicio;
+            }
+            else
+            {
+                FechaInicioInvalidaException Exception = new FechaInicioInvalidaException("La Fecha de Inicio de evento ingresada es inválida");
+                throw Exception;
+            }
+
+
+        }
+
+        public void ModificarEvento(TipoFrecuencia pFrecuencia)
+        {
+            this.Frecuencia = pFrecuencia;
+        }
+
+    }
+}
+    
